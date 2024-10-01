58d6a2bdc7ba3d31db1fe2d5da6d62198b5f2684
Fr1ba <laub99@gmail.com>
2024-10-01 12:12:46 +0200

    Begynnelsen på Person superclass

---
+++ OAPteam01/src/main/java/OAPteam01/OAPteam01/Person.java
@@ -0,0 +1,12 @@
+package OAPteam01.OAPteam01;
+
+/**
+* A superclass for the customer and actor class
+* 
+* @author Erica Laub Varpe
+*/
+public class Person {
+	private String firstName;
+	private String lastName;
+
+}
