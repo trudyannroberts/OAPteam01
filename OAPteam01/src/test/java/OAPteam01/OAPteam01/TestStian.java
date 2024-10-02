5d4d7d193023b043626e792a0766bc0d63a05771
SSRosvald <ssrosvald@gmail.com>
2024-10-02 12:55:52 +0200

    Create TestStian.java
    Prøver å teste hvorvidt jeg kan pushe

---
+++ OAPteam01/src/test/java/OAPteam01/OAPteam01/TestStian.java
@@ -0,0 +1,9 @@
+package OAPteam01.OAPteam01;
+// Denne er kun for å teste hvorvidt Stian er i stand til å pushe fra Eclipse...
+public class TestStian {
+
+	public TestStian() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
