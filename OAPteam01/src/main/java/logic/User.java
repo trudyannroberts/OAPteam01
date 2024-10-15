17606b2ab4ed2c3b942a1ef5b09d9869da5d406b
Trudy <trudyannroberts@gmail.com>
2024-10-15 11:21:48 +0200

    Laget User class, tilpasset i andre klasser

---
+++ OAPteam01/src/main/java/logic/User.java
@@ -0,0 +1,70 @@
+package logic;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+    private String password; // You could also store a hashed version of the password
+
+    // Constructor
+    public User(String firstName, String lastName, String email, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password; // In practice, you would return a hashed version or not expose this directly
+    }
+
+    // Setters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
