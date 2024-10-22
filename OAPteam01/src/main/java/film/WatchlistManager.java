14a9f34e0133270072fcf9161ac7225281aecd46
SSRosvald <ssrosvald@gmail.com>
2024-10-22 10:44:04 +0200

    Update WatchlistManager.java
    Lagt til metoder i WatchlistManager. Håper dette fungerer til å begynne med. Skal siden starte ui og ux

--- OAPteam01/src/main/java/film/WatchlistManager.java
+++ OAPteam01/src/main/java/film/WatchlistManager.java
@@ -3,8 +3,34 @@ package film;
 /**
  * @author Stian
  */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class WatchlistManager {
 	
+	//Hashmap to connect a profile to a list of films watched
+	private HashMap<String, List<String>> profileWatchlist;
 	
+	public WatchlistManager() {
+		profileWatchlist = new HashMap<>();
+	}
+	
+	// add a film to a profile's watchlist.
+	public void addWatchedMovie(String profileId, String movieTitle) {
+		profileWatchlist.putIfAbsent(profileId, new ArrayList<>());
+		profileWatchlist.get(profileId).add(movieTitle);
+	}
+
+	//Check if a film has been watched by a specific profile
+	public boolean hasWatchedMovie(String profileId, String movieTitle) {
+		return profileWatchlist.containsKey(profileId) && profileWatchlist.get(profileId).contains(movieTitle);
+	}
+	
+	// return the entire list of movies watched by a profile'
+	public List<String> getWatchedMovies(String profileId) {
+		return profileWatchlist.getOrDefault(profileId, new ArrayList<>());
+	}
 
 }
