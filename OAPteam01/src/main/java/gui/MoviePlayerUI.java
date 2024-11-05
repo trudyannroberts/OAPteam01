9928f8b4d9f5a09813becf39ba81d9070e44e13e
StineSkroder <stine.skroder@vikenfiber.no>
2024-11-05 11:52:38 +0100

    removed unused imports

--- OAPteam01/src/main/java/gui/MoviePlayerUI.java
+++ OAPteam01/src/main/java/gui/MoviePlayerUI.java
@@ -1,20 +1,9 @@
 package gui;
 
-import db.DatabaseConnection;
-import gui.ReviewGUI;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter; //lagt til
-import java.awt.event.WindowEvent; //lagt til 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.event.WindowAdapter; 
+import java.awt.event.WindowEvent;  
 import javax.swing.plaf.basic.BasicSliderUI;
 import javax.swing.border.EmptyBorder;
 import javax.swing.Timer;
